667c835292ea247a90c4252e3423a8c29bbc22dd
fabiocentrella1994@gmail.com <fabiocentrella1994@gmail.com>
2020-09-20 18:58:10 +0100

    changed database configuration

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql");
 
 let dbConfig = {
-    host     : 'localhost',
-    database : 'Connect_E',
-    user     : 'root',
-    password : 'Java\Spring)RDS)94',
+    host     : 'connect-e.cxu3dbx5ys9k.eu-west-2.rds.amazonaws.com',
+    user     : 'OpenClassrooms',
+    password : 'VueJS(NodeJS)94',
+    port: '3306',
     multipleStatements: true
   };
 
 let connection = mysql.createConnection(dbConfig);
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected To Local MySQL!");
-  })
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to AWS MySQL.');
+});
 
 module.exports = connection
+
+

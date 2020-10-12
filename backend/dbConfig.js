09a74e56b14e0617cfc4a9e99adeea587c8ff345
fabiocentrella1994@gmail.com <fabiocentrella1994@gmail.com>
2020-10-12 20:26:44 +0100

    environment variables have been set

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,5 +1,20 @@
 const mysql = require("mysql");
 
+// let dbConfig = ({
+//   host     : 'connect-e.cxu3dbx5ys9k.eu-west-2.rds.amazonaws.com',
+//   user     : process.env.AWS_RDS_USER,
+//   password : process.env.AWS_RDS_PASSWORD,
+//   port     : '3306'
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('Database connection failed: ' + err.stack);
+//     return;
+//   }
+//   console.log('Connected to AWS MySQL.');
+// });
+
 let dbConfig = {
     host     : 'localhost',
     database : 'Connect_E',
